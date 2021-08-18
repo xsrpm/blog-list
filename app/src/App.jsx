b52c6b87bd0aff9dbffb737f3b609fc20afa0e3f
César Palma Morante <cemp2703@gmail.com>
2021-08-18 09:33:17 -0500

    add react router dom to application

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { initializeLogin } from './actions/loginAction'
 import AppBlog from './components/AppBlog'
 import LoginContainer from './components/LoginContainer'
 import Notification from './components/Notification'
 
-const AppLogin = () => {
+const AppRouter = () => {
   const signedUser = useSelector((state) => state.signedUser)
-  const blogApp = () => <AppBlog />
+  const routes = () => (
+    <Switch>
+      <Route path='/'>
+        <AppBlog />
+      </Route>
+    </Switch>
+  )
 
   const login = () => (
     <LoginContainer>
@@ -15,14 +22,14 @@ const AppLogin = () => {
     </LoginContainer>
   )
 
-  return <>{signedUser ? blogApp() : login()}</>
+  return <Router>{signedUser ? routes() : login()}</Router>
 }
 
 const App = () => {
   const dispatch = useDispatch()
   const signedUser = JSON.parse(window.localStorage.getItem('signedUser'))
   dispatch(initializeLogin(signedUser))
-  return <AppLogin />
+  return <AppRouter />
 }
 
 export default App
