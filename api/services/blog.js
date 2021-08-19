d5420f7dacb2d6ebdcbe3a05adb2cd0c26bbca38
César Palma Morante <cemp2703@gmail.com>
2021-08-18 20:36:07 -0500

    fix update, like blog

--- api/services/blog.js
+++ api/services/blog.js
@@ -4,7 +4,12 @@ const Exception = require('../utils/exception')
 
 const createBlogService = async (title, url, userId) => {
   const user = await getUserById(userId)
-  if (user === null) throw new Exception('Unauthorized', `the user with the id:'${userId}' does not exist`)
+  if (user === null) {
+    throw new Exception(
+      'Unauthorized',
+      `the user with the id:'${userId}' does not exist`
+    )
+  }
   const savedBlog = await createBlog(title, url, user.username, userId)
   user.blogs = user.blogs.concat(savedBlog._id)
   // console.log('user', user)
@@ -15,21 +20,41 @@ const createBlogService = async (title, url, userId) => {
 const updateBlogByIdService = async (blogId, title, url, likes, userId) => {
   const user = await getUserById(userId)
   const blog = await getBlogById(blogId)
-  if (blog === null) throw new Exception('BadRequest', `the blog with the id:'${blogId}' does not exist`)
-  const savedBlog = await updateBlogById(blogId, { title: (title || blog.title), author: user.username, url: (url || blog.url), likes: (likes || blog.likes) })
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  if (blog === null) {
+    throw new Exception(
+      'BadRequest',
+      `the blog with the id:'${blogId}' does not exist`
+    )
+  }
+  const savedBlog = await updateBlogById(blogId, {
+    title: title || blog.title,
+    author: user.username,
+    url: url || blog.url,
+    likes: likes || blog.likes
+  })
   return savedBlog
 }
 
 const updateLikeByIdService = async (blogId, userId) => {
   const user = await getUserById(userId)
   const blog = await getBlogById(blogId)
-  if (blog === null) throw new Exception('BadRequest', `the blog with the id:'${blogId}' does not exist`)
-  const savedBlog = await updateBlogById(blogId, { title: blog.title, author: user.username, url: blog.url, likes: (blog.likes + 1) })
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  if (blog === null) {
+    throw new Exception(
+      'BadRequest',
+      `the blog with the id:'${blogId}' does not exist`
+    )
+  }
+  const savedBlog = await updateBlogById(blogId, {
+    title: blog.title,
+    author: user.username,
+    url: blog.url,
+    likes: blog.likes + 1
+  })
   return savedBlog
 }
 
-module.exports = { createBlogService, updateBlogByIdService, updateLikeByIdService }
+module.exports = {
+  createBlogService,
+  updateBlogByIdService,
+  updateLikeByIdService
+}
